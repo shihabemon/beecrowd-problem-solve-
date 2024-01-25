bd0f5d994fa293719f24784f4594bd97041da26f
Shihabul Islam Emon <151238840+shihabemon@users.noreply.github.com>
2024-01-25 14:04:08 +0600

    bee 1064.py

--- bee 1064.py
+++ bee 1064.py
@@ -1,11 +1,13 @@
 P_number =  0
+sum_number = 0
 
-for i in range(6):
+for _ in range(6):
     x = float(input())
     if x > 0 :
         P_number += 1
+        sum_number += x
 
-print(P_number)
+average_all_positive = sum_number / P_number 
 
-P_number1 = x > 0 + P_number
-print(P_number1)
+print("{:} valores positivos".format(P_number))
+print("{:.1f}".format(average_all_positive))
